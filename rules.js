11e2a71e495b90253683a9a5741977b21e54b608
teisuru <31881306+teisuru@users.noreply.github.com>
2025-09-02 19:20:55 +0200

    fix rollback bug when no change in log

--- rules.js
+++ rules.js
@@ -8524,7 +8524,8 @@ function restore_rollback(index) {
     let save_seed = game.seed
     let save_log = game.log
     game = rollback_state[index]
-    save_log.length = game.log
+    if (game.log.length !== save_log.length)
+        save_log.length = game.log
     game.log = save_log
     game.undo = [] // Rollback always wipes out the undo stack
     game.rollback = save_rollback.slice(0, index+1) // Keep older (and restored) rollback points
