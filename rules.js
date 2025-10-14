bf7eb6e1648580ed35b13543f309b10102ecf22e
Nathan Forget <nforget@users.noreply.github.com>
2025-10-13 21:50:09 -0700

    Fix belgrade replacements and add extra FR Mutiny warning (#55)
    * Fixed bug SR army could be replaced Belgrade with CP controlled Nis
    
    * Additional Fr mutiny notification
    
    * Switch to set_add and set_delete
    
    Co-authored-by: Daniil Klipov <danklipov97@gmail.com>

--- rules.js
+++ rules.js
@@ -1666,6 +1666,8 @@ states.confirm_mo = {
     prompt() {
         if (game[active_faction()].mo === NONE)
             view.prompt = `Turn ${game.turn}: No mandated offensive.`
+        else if (game.events.french_mutiny > 0 && game[active_faction()].mo === FRANCE)
+            view.prompt = `Turn ${game.turn}: French mutiny (Mandated offensive for ${nation_name(game[active_faction()].mo)}).`
         else
             view.prompt = `Turn ${game.turn}: Mandated offensive for ${nation_name(game[active_faction()].mo)}.`
         gen_action_next()
@@ -6647,16 +6649,16 @@ function get_army_replacement_spaces(p) {
         const belgian_spaces = [BRUSSELS, ANTWERP, OSTEND]
         for (let s of belgian_spaces) {
             if (is_controlled_by(s, AP) && is_unit_supplied_in(p, s) && !is_fully_stacked(s, AP))
-                spaces.push(s)
+                set_add(spaces, s)
         }
         if (spaces.length === 0) {
             if (is_controlled_by(CALAIS, AP) && is_unit_supplied_in(p, CALAIS) && !is_fully_stacked(CALAIS, AP))
-                spaces.push(CALAIS)
+                set_add(spaces, CALAIS)
         }
         return spaces
     }
 
-    spaces = spaces.concat(get_available_reinforcement_spaces(p))
+    get_available_reinforcement_spaces(p).forEach((s) => set_add(spaces, s))
 
     if (data.pieces[p].nation === SERBIA) {
         //  Also 17.1.5: Serbian Army units may be recreated at Salonika if the Salonika or Greece Neutral Entry Event
@@ -6664,13 +6666,12 @@ function get_army_replacement_spaces(p) {
         //  following normal reinforcement restrictions.
         if (game.events.salonika > 0 || game.events.greece_entry > 0) {
             if (is_controlled_by(SALONIKA, AP) && is_unit_supplied_in(p, SALONIKA) && !is_fully_stacked(SALONIKA, AP))
-                spaces.push(SALONIKA)
+                set_add(spaces, SALONIKA)
         }
 
         // Exception: Serb armies may not be recreated at Belgrade if Nis is under CP control.
-        if (is_controlled_by(NIS, CP)) {
-            array_remove_item(spaces, BELGRADE)
-        }
+        if (is_controlled_by(NIS, CP))
+            set_delete(spaces, BELGRADE)
     }
 
     return spaces
